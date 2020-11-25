f5a568626f94520f770dbeb0361a41993f1993b5
hannah-scott <hannahscott@tutanota.com>
2020-11-25 09:01:55 +0000

    Changed to prompt

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import numpy as np
 SCREEN_WIDTH=1920
 SCREEN_HEIGHT=1080
 
-h = 'eee3e7'
-rbg = tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
+h = input("Enter hex code: ").strip('#')
+rgb = tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
 
 array = np.zeros([SCREEN_HEIGHT, SCREEN_WIDTH, 3], dtype=np.uint8)
-array[:,:] = rbg
+array[:,:] = rgb
 
 img = Image.fromarray(array)
-img.save('wallcolor_{}_{}_{}.png'.format(rbg[0], rbg[1], rbg[2]))
+img.save('wallcolor_{}_{}_{}.png'.format(rgb[0], rgb[1], rgb[2]))
